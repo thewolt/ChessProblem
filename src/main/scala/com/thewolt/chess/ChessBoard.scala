05fd1baea228207c435f3c317f2ece786db6af81
thewolt <thewolt@gmail.com>
2010-11-19 00:08:24 +0100

    initial commit for memory efficient solution

---
+++ src/main/scala/com/thewolt/chess/ChessBoard.scala
@@ -0,0 +1,246 @@
+package com.thewolt.chess
+
+import collection.mutable.Stack
+import collection.mutable.Set
+
+trait IPos {
+  type P <: IPos
+  val x: Int
+  val y: Int
+  def withDir(d: IPos): P
+  def toOffset: Int
+}
+
+trait Piece {
+  val moves: Iterable[IPos]
+  val letter: String
+}
+//class Data(val map: List[(Int, String)]) {
+//  override def hashCode = map.sortBy( a => a._1 ).foldLeft(0) { (a,b) => a << 3 | b._1 }
+//  override def equals(o: Any) = {
+//    o match {
+//      case d: Data => map.sortBy(a => a._1).equals(d.map.sortBy(a => a._1))
+//      case _ => false
+//    }
+//  }
+//}
+object Data {
+    /* returns data and found */
+  def addChild(children: List[Data], d: Data): (Data, Boolean) = {
+    children match {
+      case Nil => (d, true)
+      case used :: rest => if(d == used) (used, false) else addChild(rest, d)
+    }
+  }
+}
+
+case class Data(index: Int, letter: Char) {
+  var children : List[Data] = Nil
+
+  def withPiece2(pos: IPos, letter: String): (Data,Boolean) = {
+    withPiece(pos.toOffset, letter.charAt(0))
+  }
+  def withPiece(pos: Int, letter: Char): (Data,Boolean) = {
+    val (data, b) = Data.addChild(children, Data(pos, letter))
+    if(b) {
+      children = data :: children
+    }
+    (data,b)
+  }
+}
+
+class CheckBoard(width: Int, height: Int) {
+  import math._
+
+  case class Pos(x: Int, y: Int) extends IPos {
+    type P = Pos
+    def withDir(d: IPos) = copy( x = d.x + x , y = d.y + y)
+    def toOffset = y * width + x
+  }
+
+  def fromOffset(i: Int) = Pos(i % width, i / width)
+  def allPossibleMoves = for( x <- -width to width ; y <- -height to height) yield Pos(x,y)
+
+
+  object King extends Piece {
+    val moves = allPossibleMoves.filter { p => max(abs(p.x), abs(p.y)) == 1 }
+    val letter = "K"
+  }
+
+  object Rooke extends Piece {
+    val moves = allPossibleMoves.filter { p => p.x == 0 || p.y == 0 }
+    val letter = "R"
+  }
+
+  object Bishop extends Piece {
+    val moves = allPossibleMoves.filter { p => abs(p.x) == abs(p.y) }
+    val letter = "B"
+  }
+
+  object Queen extends Piece {
+    val moves = allPossibleMoves.filter { p => abs(p.x) == abs(p.y) || p.x == 0 || p.y == 0 }
+    val letter = "Q"
+  }
+
+  object Knight extends Piece {
+    val moves = allPossibleMoves.filter { p => abs(p.x) * abs(p.y) == 2 }
+    val letter = "N"
+  }
+
+  type BRepr = String
+
+  abstract class Square
+  case object Empty extends Square
+  case object Check extends Square
+  case class WithPiece(p: Piece) extends Square
+
+  case class BoardCons(parent: CheckBoardInstance, piece: Piece, pos: Pos)
+
+  class CheckBoardInstance( val layout: Array[Square], info: Option[BoardCons]) {
+    require(layout.size == width * height)
+
+    def this() = this(Array.fill(width * height)(Empty), None)
+    def this(layout: Array[Square]) = this(layout, None)
+    //def this( sqs: Square* ) = this(sqs.toArray)
+
+    lazy val repr : List[(Int, String)] = info match {
+      case None => Nil
+      case Some(BoardCons(parent, piece, pos)) => (pos.toOffset -> piece.letter) :: parent.repr
+    }
+
+    def usedRepr = {
+      val buf = new StringBuilder
+      repr.toList.sortBy(a => a._1).foreach { case (k,v) => buf.append(k).append(v) }
+      buf.toString
+    }
+    //def usedRepr : BRepr = new Data(repr)
+
+
+    val inBoard: Pos => Boolean = (p: Pos) => p.x >= 0 && p.x < width && p.y >=0 && p.y < height
+
+    def pieceMoves(piece: Piece, pos: Pos) = piece.moves.map { p => pos.withDir(p) }.filter( inBoard )
+
+    def atPos(pos: Pos): Square = layout(pos.toOffset)
+
+    def withPieceAt(piece: Piece, pos: Pos): CheckBoardInstance = {
+      _withPieceAt(piece, pos, pieceMoves(piece, pos))
+    }
+
+    def _withPieceAt(piece: Piece, pos: Pos, moves: Iterable[Pos]): CheckBoardInstance = {
+      val newLayout = layout.clone
+      for( p <- moves) {
+//        val newVal = atPos(p) match {
+//          case Empty => Check
+//          case Check => Check
+//          case _ => error("cannot place: " + piece + " at " + pos + " unexpected pos at:" + p)
+//        }
+//        newLayout(p.toOffset) = newVal
+        newLayout(p.toOffset) = Check
+      }
+      newLayout(pos.toOffset) = WithPiece(piece)
+      new CheckBoardInstance(newLayout, Some(BoardCons(this, piece, pos)))
+    }
+
+    def place(piece: Piece): Iterable[CheckBoardInstance] = availablePos.flatMap { p =>
+      val moves = pieceMoves(piece, p)
+      if(_canPieceStay(piece, p, moves)) {
+        Some(_withPieceAt(piece, p, moves))
+      } else {
+        None
+      }
+    }
+
+    /**
+     * means does not check another piece
+     */
+    def canPieceStay(piece: Piece, pos: Pos): Boolean = _canPieceStay(piece, pos, pieceMoves(piece, pos))
+
+    def _canPieceStay(piece: Piece, pos: Pos, moves: Iterable[Pos]): Boolean = moves.forall { p =>
+      val i = atPos(p)
+      (i eq Empty) || (i eq Check) 
+    }
+
+    def availablePos: Iterable[Pos] = layout.zipWithIndex.flatMap { case (sq,i) => if(sq == Empty) Some(fromOffset(i)) else None }
+
+    override def equals(obj: Any) = {
+      obj match {
+        case other: CheckBoardInstance => List(layout: _*) == List(other.layout :_*)
+        case _ => false
+      }
+    }
+
+
+    override def toString = "Check[\n" + (0 until height).map( y =>
+      (0 until width).map( x =>
+        atPos(Pos(x,y)) match {
+          case Empty => "."
+          case Check => "*"
+          case WithPiece(p) => p.letter
+        }).mkString
+      ).mkString("\n") + "\n]"
+  }
+
+  object EmptyBoard extends CheckBoardInstance()
+
+  case class Level( level: Int, cur: CheckBoardInstance)
+
+  def filterRepr(repr: List[(Int, String)]) : List[(Int, String)] = {
+    repr match {
+      case Nil => Nil
+      case a :: Nil => a :: Nil
+      case (pos1, letter1) :: (pos2, letter2) :: rest if pos1 > pos2 && letter1 == letter2 => filterRepr((pos2, letter2) :: (pos1, letter1) :: rest)
+      case a :: rest => a :: filterRepr(rest)
+    }
+  }
+
+
+
+  def place(_pieces: Piece*): Int = {
+    val pieces = Vector( _pieces : _*)
+    val stack = new Stack[Level]()
+    stack.push(Level(0, EmptyBoard))
+    var count = 0
+    var res = List[Data]()
+    while( !stack.isEmpty) {
+      val Level(level, board) = stack.pop
+
+      if(level == pieces.size) {
+        val root :: rest = filterRepr(board.repr)
+        val (resRoot, resRootAdd) = Data.addChild(res, Data(root._1, root._2.charAt(0)))
+        if(resRootAdd) {
+          res ::= resRoot
+        }
+        val (_, newBranch) = rest.foldLeft((resRoot, false)) { case ((data : Data,b : Boolean), (pos : Int,letter: String)) =>
+          val (newData, newB) = data.withPiece(pos, letter.charAt(0))
+          (newData , newB || b)
+        }
+        if(newBranch) {
+          count += 1
+        }
+        //println("found: " + board)
+        if(count % 1000 == 0) {
+          println("got res.size="+count)
+        }
+      } else {
+        val curPiece = pieces(level)
+        board.place(curPiece).foreach( newBoard =>
+          stack.push(Level(level+1, newBoard))
+        )
+      }
+    }
+    count
+  }
+
+}
+
+
+
+
+object Main {
+  def main(args: Array[String]) {
+    val board = new CheckBoard(7, 7)
+    import board._
+    
+    println( board.place( Queen, Queen, Bishop, Bishop, King, King, Knight) )
+  }
+}
---
+++ src/test/scala/com/thewolt/chess/CheckBoardTest.scala
@@ -0,0 +1,61 @@
+package com.thewolt.chess
+
+import org.scalatest._
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: thewolt
+ * Date: Nov 14, 2010
+ * Time: 9:57:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class CheckBoardTest extends FunSuite {
+  val board = new CheckBoard(3, 3)
+  import board._
+
+  test("board instance should list available pos") {
+    val i = new CheckBoardInstance( Array(
+      WithPiece(King), Check, Empty,
+      Check, Check, Empty,
+      Empty, Empty, Empty
+    ))
+    assert( i.availablePos.toSet === Set(Pos(2, 0), Pos(2, 1), Pos(0, 2), Pos(1, 2), Pos(2, 2)))
+  }
+
+  test("board instance should generate an instance with another piece") {
+    val i = new CheckBoardInstance()
+    assert( i.withPieceAt(King, Pos(0, 0)) === new CheckBoardInstance(Array(
+        WithPiece(King), Check, Empty,
+        Check, Check, Empty,
+        Empty, Empty, Empty
+      )))
+  }
+
+  test("board instance should give all move of a piece") {
+    val i = new CheckBoardInstance()
+    assert( i.pieceMoves(King, Pos(0, 0)).toSet === Set(Pos(0, 1), Pos(1, 1), Pos(1, 0)))
+  }
+
+  test("board instance should tell if a piece can stay") {
+    val i = (new CheckBoardInstance).withPieceAt(King, Pos(0, 0))
+    assert( i.canPieceStay(Rooke, Pos(2, 0)) === false)
+    assert( i.canPieceStay(Rooke, Pos(2, 1)) === true )
+  }
+
+  test("board instance can give a list of all possible place for a piece") {
+    val i = (new CheckBoardInstance).withPieceAt(King, Pos(0, 0))
+    assert( i.place(Rooke).toSet === Set(Pos(2, 1), Pos(2, 2), Pos(1, 2)).map( i.withPieceAt(Rooke, _)))
+  }
+
+  test("board can place 2 king and a rook") {
+    assert( board.place(King, King, Rooke) === 4)
+//    assert( board.place(King, King, Rooke) === Set(
+//      EmptyBoard.withPieceAt(King, Pos(0, 0)).withPieceAt(King, Pos(0, 2)).withPieceAt(Rooke, Pos(2, 1)),
+//      EmptyBoard.withPieceAt(King, Pos(0, 0)).withPieceAt(King, Pos(2, 0)).withPieceAt(Rooke, Pos(1, 2)),
+//      EmptyBoard.withPieceAt(King, Pos(0, 2)).withPieceAt(King, Pos(2, 2)).withPieceAt(Rooke, Pos(1, 0)),
+//      EmptyBoard.withPieceAt(King, Pos(2, 0)).withPieceAt(King, Pos(2, 2)).withPieceAt(Rooke, Pos(0, 1))
+//
+//      ).map(_.usedRepr) )
+  }
+}
